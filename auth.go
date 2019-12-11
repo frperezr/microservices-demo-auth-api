f0e2dbbfd9888c7066bb1395d52c9b525cbaa82b
Francisco Pérez <frperezr93@gmail.com>
2019-12-10 23:58:57 -0300

    add auth model

--- auth.go
+++ auth.go
@@ -1,1 +1,21 @@
 package auth
+
+import (
+	user "github.com/frperezr/noken-test/src/users-api"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Token is a jwt returned by noken auth service
+type Token struct {
+	UserID string `json:"user_id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	*jwt.StandardClaims
+}
+
+// Service ...
+type Service interface {
+	Login(email, password string) (string, error)
+	Signup(user *user.User) (string, error)
+}
