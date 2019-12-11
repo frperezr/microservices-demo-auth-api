6020afd267b7516b7ae54610ae72e00c4f778604
Francisco Pérez <frperezr93@gmail.com>
2019-12-10 23:59:11 -0300

    add auth server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,44 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	pb "github.com/frperezr/noken-test/pb"
+
+	authSvc "github.com/frperezr/noken-test/src/auth-api/rpc/auth"
+
+	"google.golang.org/grpc/reflection"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := os.Getenv("AUTH_PORT")
+	if port == "" {
+		log.Fatal("Env variable AUTH_PORT must be defined")
+	}
+
+	srv := grpc.NewServer()
+	svc := authSvc.New()
+
+	pb.RegisterAuthServiceServer(srv, svc)
+	reflection.Register(srv)
+
+	log.Println("Starting auth service...")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	if err != nil {
+		log.Printf("Failed to list: %v", err)
+		os.Exit(1)
+	}
+
+	log.Println(fmt.Sprintf("Auth service running, Listening on: %v", port))
+
+	if err := srv.Serve(lis); err != nil {
+		log.Printf("Fatal to serve: %v", err)
+		os.Exit(1)
+	}
+}
