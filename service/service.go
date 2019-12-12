5f0fbd104c91099605145c1db0eac8134e0f0098
Francisco Pérez <frperezr93@gmail.com>
2019-12-12 01:18:18 -0300

    update service

--- service/service.go
+++ service/service.go
@@ -1,23 +1,28 @@
 package service
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/frperezr/noken-test/lib/clients/users"
 	"github.com/frperezr/noken-test/src/auth-api"
 	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc"
+
+	pb "github.com/frperezr/noken-test/pb"
 
 	user "github.com/frperezr/noken-test/src/users-api"
 )
 
 // AuthSvc ...
 type AuthSvc struct {
-	Client *users.Client
+	Client pb.UserServiceClient
+	Conn   *grpc.ClientConn
 }
 
 // New ...
@@ -32,32 +37,44 @@ func New() *AuthSvc {
 		log.Fatal("[Auth Service][New Service][Error] missing USERS_PORT env variable")
 	}
 
+	addr := fmt.Sprintf("%v:%v", usersHost, usersPort)
+
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(fmt.Sprintf("[Auth Service][New Service][Error] %v", err))
+	}
+
 	return &AuthSvc{
-		Client: &users.Client{
-			UsersHost: usersHost,
-			UsersPort: usersPort,
-		},
+		Client: pb.NewUserServiceClient(conn),
+		Conn:   conn,
 	}
 }
 
 // Login ...
 func (as *AuthSvc) Login(email, password string) (string, error) {
-	user, err := as.Client.GetByEmail(email)
+	res, err := as.Client.GetByEmail(context.Background(), &pb.GetUserByEmailRequest{
+		Email: email,
+	})
 	if err != nil {
-		return "", errors.New(err.Error())
+		return "", err
 	}
+
+	if res.GetError() != nil {
+		return "", errors.New(res.GetError().GetMessage())
+	}
+
+	user := res.GetData()
 
 	expiresAt := time.Now().Add(time.Minute * 100000).Unix()
 
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+	err = bcrypt.CompareHashAndPassword([]byte(user.GetPassword()), []byte(password))
+	if err != nil {
 		return "", errors.New("Invalid login credentials. Please try again")
 	}
 
 	tk := &auth.Token{
-		UserID: user.ID,
+		UserID: user.Id,
 		Name:   user.Name,
-		Email:  user.Email,
 		StandardClaims: &jwt.StandardClaims{
 			ExpiresAt: expiresAt,
 		},
@@ -65,35 +82,44 @@ func (as *AuthSvc) Login(email, password string) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
 
-	tokenString, error := token.SignedString([]byte("secret"))
-	if error != nil {
-		fmt.Println(error)
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return "", err
 	}
 
 	return tokenString, nil
 }
 
 // Signup handles the creation of a new user
-func (as *AuthSvc) Signup(user *user.User) (string, error) {
-	if user == nil {
+func (as *AuthSvc) Signup(u *user.User) (string, error) {
+	if u == nil {
 		return "", errors.New("missing user param")
 	}
 
-	pass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	pwd := u.Password
+
+	pass, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return "", errors.New(err.Error())
+		return "", err
 	}
 
-	user.Password = string(pass)
+	u.Password = string(pass)
+	u.Email = strings.ToLower(u.Email)
 
-	usr, err := as.Client.Create(user)
+	res, err := as.Client.Create(context.Background(), &pb.CreateUserRequest{
+		Data: u.ToProto(),
+	})
 	if err != nil {
-		return "", errors.New(err.Error())
+		return "", err
 	}
 
-	token, err := as.Login(usr.Email, usr.Password)
+	if res.GetError() != nil {
+		return "", errors.New(res.GetError().GetMessage())
+	}
+
+	token, err := as.Login(u.Email, pwd)
 	if err != nil {
-		return "", errors.New(err.Error())
+		return "", err
 	}
 
 	return token, nil
